72d07fab8c601572f7cf963a4a5aa0b695410ec4
zcalhoun <zcalhoun.ny@gmail.com>
2022-06-22 14:04:46 -0400

    Added logging to datasets file.

--- src/datasets.py
+++ src/datasets.py
@@ -1,9 +1,11 @@
 import os
+import logging
 
 from tasks.solar import SolarPVDataset
 
 
-def load(task, test_size):
+def load(task):
+    logging.debug(f"In datasets, the task {task} is being loaded.")
     task_map = {
         "solar": _load_solar_data,
     }
@@ -14,13 +16,24 @@ def load(task, test_size):
 
 # TODO -- finish loading the solar data by splitting
 # into a train/test split.
-def _load_solar_data(test_size=0.2):
+def _load_solar_data():
     # Split the data into a train and test set
-    list(filter(lambda x: x[-3:] == "tif", os.listdir(path)))
+    data_path = "/data/users/zdc6/data/SolarPV/fresno"
+    files = list(filter(lambda x: x[-3:] == "tif", os.listdir(data_path)))
+    logging.debug(f"There are {len(files)} files in the Frenso dataset.")
+    shape_file = "/data/users/zdc6/data/SolarPV/SolarArrayPolygons.geojson"
+
+    # Split files into two lists with an 80/20 split.
+    # In this case, put every fifth file into the test set.
+    mask = np.arange(0, len(files)) % 5 == 0
+    test_files = files[mask]
+    train_files = files[~mask]
 
     # Load the training dataset
-
+    logging.debug("Creating the training dataset.")
+    train_dataset = SolarPVDataset(data_path, train_files, shape_file)
     # Load the test dataset
-
+    logging.debug("Creating the test dataset.")
+    test_dataset = SolarPVDataset(data_path, test_files, shape_file)
     # Return the training and test dataset
-
+    return train_dataset, test_dataset
