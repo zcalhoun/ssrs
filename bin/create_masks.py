98fd71314c2e743e32a31106295c482714ab7db3
zcalhoun <zcalhoun.ny@gmail.com>
2022-06-29 12:52:58 -0400

    Cleaned up logic for handling polygons

--- bin/create_masks.py
+++ bin/create_masks.py
@@ -31,32 +31,26 @@ def main(dump_path):
         image_name = fn.split(".")[0]
         # Get polygons for this image.
         try:
-            polygons = image_map.iloc[image_name]
+            polygons = image_map.loc[image_name]
         except KeyError:
             print(f"No polygons found for image {image_name}")
             continue
 
-        if len(polygons) == 0:
-            # Don't create an empty image.
-            continue
-        elif len(polygons) < 2:
-            # Create new blank image and drawing
-            new_img = Image.new(str(1), (5000, 5000), color=0)
-            mask = ImageDraw.Draw(new_img)
+        # Create new blank image and drawing
+        new_img = Image.new(str(1), (5000, 5000), color=0)
+        mask = ImageDraw.Draw(new_img)
 
-            vertices = polygon_vertices.loc[int(polygons["polygon_id"])]
-            vertices = [i for i in vertices[1:] if not (pd.isna(i))]
-            mask.polygon(vertices, fill=1)
-        else:
-            # Create new blank image and drawing
-            new_img = Image.new(str(1), (5000, 5000), color=0)
-            mask = ImageDraw.Draw(new_img)
-
-            # Loop through all of the polygons:
+        # Loop through all of the polygons:
+        try:
             for idx, polygon in polygons.iterrows():
                 vertices = polygon_vertices.loc[int(polygon["polygon_id"])]
                 vertices = [i for i in vertices[1:] if not (pd.isna(i))]
                 mask.polygon(vertices, fill=1)
+        except AttributeError:
+            # If there is no attribute, don't iterate through
+            vertices = polygon_vertices.loc[int(polygons["polygon_id"])]
+            vertices = [i for i in vertices[1:] if not (pd.isna(i))]
+            mask.polygon(vertices, fill=1)
 
         print("Saving image...")
         new_img.save(dump_path + fn)
