ef54ec69a469b9dfa53834bc71ecdd846a2d9b5c
Saad Lahrichi <sl636@pizer.egr.duke.edu>
2022-07-07 12:18:07 -0400

    ADD: setup for building task

--- src/datasets.py
+++ src/datasets.py
@@ -6,12 +6,14 @@ import joblib
 from torchvision import transforms
 
 from .tasks.solar import SolarPVDataset
+from .tasks.building import BuildingSegmentationDataset
 
 
 def load(task):
     logging.debug(f"In datasets, the task {task} is being loaded.")
     task_map = {
         "solar": _load_solar_data,
+        "building": _load_building_data
     }
     if task not in task_map:
         raise ValueError
@@ -57,6 +59,51 @@ def _load_solar_data(data_path, mask_path, files):
     # Load the test dataset
     logging.debug("Creating the test dataset.")
     test_dataset = SolarPVDataset(
+        data_path, test_files, mask_path, transform=test_transform
+    )
+    # Return the training and test dataset
+    return train_dataset, test_dataset
+
+
+def _load_building_data(data_path, mask_path, files):
+    # Split the data into a train and test set
+    # data_path = "/scratch/zach/solar-pv/"
+    # mask_path = "/scratch/zach/mask_tensors/"
+    # files = joblib.load("/scratch/zach/train_test_split.joblib")
+    # files = np.array(list(filter(lambda x: x[-3:] == "tif", os.listdir(data_path))))
+    logging.debug(f"There are {len(files)} files in the Frenso dataset.")
+
+    # Split files into two lists with an 80/20 split.
+    # In this case, put every fifth file into the test set.
+    mask = np.arange(0, len(files)) % 5 == 0
+    test_files = files['test']['empty'] + files['test']['mask']
+    train_files = files['train']['empty'] + files['train']['mask']
+
+    # TODO - find the mean and standard deviation
+    tr_normalize = transforms.Normalize(
+        mean=[0.494, 0.491, 0.499], std=[0.142, 0.141, 0.135]
+    )
+
+    # Resize shouldn't typically be necessary...but just in case,
+    # the resize operation is included.
+    train_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            tr_normalize
+        ]
+    )
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(), tr_normalize]
+    )
+
+    # Load the training dataset
+    logging.debug("Creating the training dataset.")
+    train_dataset = BuildingSegmentationDataset(
+        data_path, train_files, mask_path, transform=train_transform
+    )
+    # Load the test dataset
+    logging.debug("Creating the test dataset.")
+    test_dataset = BuildingSegmentationDataset(
         data_path, test_files, mask_path, transform=test_transform
     )
     # Return the training and test dataset
---
+++ src/tasks/building.py
@@ -0,0 +1,36 @@
+import os
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+
+class BuildingSegmentationDataset(Dataset):
+    def __init__(self, path, files, mask_path, transform=None):
+        self.path = path
+        self.files = files
+        self.mask_path = mask_path
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+
+        # Load the image
+        img_name = self.files[idx]
+
+        # Open image with PIL
+        img = Image.open(self.path+img_name)
+        mask_name = self.mask_path+img_name.split(".")[0]+'.pt'
+
+        if os.path.exists(mask_name):
+            mask = torch.load(mask_name)
+        else:
+            mask = torch.zeros((1, 224, 224))
+
+        # Apply transforms
+        if self.transform:
+            img = self.transform(img)
+
+        return img, mask.type(torch.LongTensor)
